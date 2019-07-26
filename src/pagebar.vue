e67dad988b9b86df63e921661ef25d89c1259763
RJ <rongjihuang@gmail.com>
2019-07-26 12:00:21 +0800

    Refactor pagebar component

--- src/pagebar.vue
+++ src/pagebar.vue
@@ -1,42 +1,59 @@
 <template>
-  <table class="st-pagebar">
-    <tbody>
-      <tr>
-        <td class="icon first" @click.prevent.stop="toPage(1)">⋘</td>
-        <td class="icon previous" @click.prevent.stop="toPage(Math.max(v.pageNo - 1, 1))">&lt;</td>
-        <td class="icon text">
-          <span v-if="total > 0">{{v.pageNo}}/{{pageCount}}({{total}})</span>
-          <span v-else>0</span>
-        </td>
-        <td class="icon next" @click.prevent.stop="toPage(Math.min(v.pageNo + 1, pageCount))">&gt;</td>
-        <td class="icon last" @click.prevent.stop="toPage(pageCount)">⋙</td>
-      </tr>
-    </tbody>
-  </table>
+  <span :class="classes.root">
+    <st-button :class="classes.first" @click.native.prevent.stop="toPage(1)">First</st-button>
+    <st-button
+      :class="classes.previous"
+      @click.native.prevent.stop="toPage(Math.max(v.pageNo - 1, 1))"
+    >Previous</st-button>
+    <span :class="classes.text" v-if="total > 0">{{v.pageNo}}/{{pageCount}}({{total}})</span>
+    <span :class="classes.text" v-else>0</span>
+    <st-button
+      :class="classes.next"
+      @click.native.prevent.stop="toPage(Math.min(v.pageNo + 1, pageCount))"
+    >Next</st-button>
+    <st-button :class="classes.last" @click.native.prevent.stop="toPage(pageCount)">Last</st-button>
+  </span>
 </template>
 
 <script>
+/**
+ * Events: change(newPageNo)
+ */
 import { get } from "./utils";
 import stButton from "./button.vue";
-
-const DEFAULT_PAGE_SIZE = get("simter.pagebar.pageSize", 25);
-const DEFAULT_PAGE_SIZES = get("simter.pagebar.pageSizes", [25, 50, 100]);
-
 export default {
   components: { stButton },
   props: {
     /** The current 1-base page number */
     pageNo: { type: Number, required: false, default: 0 },
     /** The maximal elements count of one page */
-    pageSize: { type: Number, required: false, default: DEFAULT_PAGE_SIZE },
+    pageSize: {
+      type: Number,
+      required: false,
+      default: () => get("simter.pagebar.pageSize", 25)
+    },
     /** The candidate page-size */
     pageSizes: {
       type: Array,
       required: false,
-      default: () => DEFAULT_PAGE_SIZES
+      default: () => get("simter.pagebar.pageSizes", [25, 50, 100])
     },
     /** The total elements count */
-    total: { type: Number, required: false, default: 0 }
+    total: { type: Number, required: true },
+    // All dom element class
+    classes: {
+      type: Object,
+      required: false,
+      default: () =>
+        get("simter.pagebar.classes", {
+          root: "st-pagebar",
+          first: "icon first",
+          previous: "icon previous",
+          text: "text",
+          next: "icon next",
+          last: "icon last"
+        })
+    }
   },
   computed: {
     pageCount() {
@@ -44,16 +61,22 @@ export default {
     }
   },
   data() {
-    return { v: { pageNo: 1 } };
+    return { v: { pageNo: undefined } };
   },
-  created() {
-    this.v.pageNo = this.pageNo;
+  watch: {
+    pageNo: {
+      immediate: true,
+      handler(value) {
+        if (value !== this.v.pageNo) this.v.pageNo = value;
+      }
+    }
   },
   methods: {
     toPage(pageNo) {
       if (pageNo !== this.v.pageNo) {
         this.v.pageNo = pageNo;
-        this.$emit("change", "changePageNo", pageNo, this.pageSize);
+        this.$emit("update:page-no", pageNo);
+        this.$emit("change", pageNo);
       }
     }
   }
@@ -62,14 +85,14 @@ export default {
 
 <style>
 .st-pagebar {
-  position: relative;
-  table-layout: fixed;
-  border-collapse: collapse;
+  display: inline-flex;
 }
-.st-pagebar > tbody > tr > td:not(.text) {
-  cursor: pointer;
-}
-.st-pagebar > tbody > tr > td.text {
+.st-pagebar > .text {
   cursor: default;
+  margin: 0 0.25rem;
+}
+.st-pagebar > :not(.text) {
+  cursor: pointer;
+  margin: 0 -1px 0 0;
 }
 </style>
