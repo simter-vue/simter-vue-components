6cffcc2067ec938f23cf3e2c71f75fd26ca665ab
RJ <rongjihuang@gmail.com>
2019-07-26 12:01:50 +0800

    Add global classes and styles config sample

---
+++ demo/global.js
@@ -0,0 +1,21 @@
+window.simter = {
+  grid: {
+    classes: {
+      root: "my-grid",
+      top: "my-top",
+      header: "my-header",
+      content: "my-content",
+      bottom: "my-bottom",
+      contentRow: {
+        root: "my-row",
+        cell: "my-cell",
+        hover: "my-hover",
+        active: "my-active",
+      }
+    },
+    styles: {
+      headerTable: { border: "1px solid gray" },
+      contentTable: { border: "1px solid gray" }
+    }
+  }
+}
