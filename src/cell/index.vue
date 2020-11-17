707920eaa31bc84cf4df0b85472f6fb102c5b3f8
RJ <rongjihuang@gmail.com>
2020-11-17 16:06:00 +0800

    Fixed cell-index component error

--- src/cell/index.vue
+++ src/cell/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <span :class="classes.root" :style="styles.root">{{index >= 0 ? index : ""}}</span>
+  <span :class="classes.root" :style="styles.root">{{content}}</span>
 </template>
 
 <script>
 // zero base row serial number cell
 
 import cellBase from "./base";
-export default { extends: cellBase };
+export default {
+  extends: cellBase,
+  computed: {
+    content() {
+      let t = this.empty
+        ? ""
+        : (this.column && this.column.pid)
+        ? this.index         // nested index
+        : this.dataRowIndex; // dataRow index
+      return t;
+    }
+  }
+};
 </script>
